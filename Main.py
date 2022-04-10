953ff5e0b37bfcf6a83ea8c2548a3d00db2053dd
michal15p <103075090+michal15p@users.noreply.github.com>
2022-04-10 16:20:40 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-
-import sys, pygame
+import pygame
 from pygame import *
 
 
@@ -36,74 +35,92 @@ def map(h,surface, color):
     pygame.draw.line(surface, color, (h*0.633, h*0.100), (h*0.633, h*0.900), 5)
     pygame.display.flip()
 
-def draw_object(h, surface, color,poz, turn):
+def check(h, poz):
     if poz[0] >= h*0.100 and poz[0] < h*0.366 and poz[1] >= h*0.100 and poz[1] < h*0.366:
+        return 0
+    elif poz[0] >= h*0.100 and poz[0] < h*0.366 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+        return 1
+    elif poz[0] >= h*0.100 and poz[0] < h*0.366 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+        return 2
+
+
+
+    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.100 and poz[1] < h*0.366:
+        return 3
+    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+        return 4
+    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+        return 5
+
+
+    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.100 and poz[1] < h*0.366:
+        return 6
+    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+        return 7
+    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+        return 8
+
+def draw_object(h, surface, color, turn):
+    if num_field == 0:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.233, h*0.233), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.165, h*0.165), (h*0.300, h*0.300), 3)
             pygame.draw.line(surface, color, (h*0.300, h*0.165), (h*0.165, h*0.300), 3)
         return 0
-    elif poz[0] >= h*0.100 and poz[0] < h*0.366 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+    elif num_field == 1:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.233, h*0.499), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.165, h*0.431), (h*0.300, h*0.568), 3)
             pygame.draw.line(surface, color, (h*0.300, h*0.431), (h*0.165, h*0.568), 3)
-        return 1
-    elif poz[0] >= h*0.100 and poz[0] < h*0.366 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+    elif num_field == 2:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.233, h*0.766), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.165, h*0.698), (h*0.300, h*0.835), 3)
             pygame.draw.line(surface, color, (h*0.300, h*0.698), (h*0.165, h*0.835), 3)
-        return 2
 
 
 
-    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.100 and poz[1] < h*0.366:
+    elif num_field == 3:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.499, h*0.233), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.430, h*0.165), (h*0.568, h*0.300), 3)
             pygame.draw.line(surface, color, (h*0.568, h*0.165), (h*0.430, h*0.300), 3)
-        return 3
-    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+    elif num_field == 4:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.499, h*0.499), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.430, h*0.430), (h*0.568, h*0.568), 3)
             pygame.draw.line(surface, color, (h*0.568, h*0.434), (h*0.434, h*0.568), 3)
-        return 4
-    elif poz[0] >= h*0.366 and poz[0] < h*0.633 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+    elif  num_field == 5:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.499, h*0.766), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.430, h*0.698), (h*0.568, h*0.835), 3)
             pygame.draw.line(surface, color, (h*0.568, h*0.698), (h*0.430, h*0.835), 3)
-        return 5
 
-    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.100 and poz[1] < h*0.366:
+
+    elif num_field == 6:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.766, h*0.233), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.698, h*0.165), (h*0.835, h*0.300), 3)
             pygame.draw.line(surface, color, (h*0.835, h*0.165), (h*0.698, h*0.300), 3)
-        return 6
-    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.366 and poz[1] < h*0.633:
+    elif num_field == 7:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.766, h*0.499), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.698, h*0.430), (h*0.835, h*0.568), 3)
             pygame.draw.line(surface, color, (h*0.835, h*0.434), (h*0.698, h*0.568), 3)
-        return 7
-    elif poz[0] >= h*0.633 and poz[0] < h*0.900 and poz[1] >= h*0.633 and poz[1] < h*0.900:
+    elif num_field == 8:
         if turn == 1:
             pygame.draw.circle(surface, color, (h*0.766, h*0.766), h*0.100, 3)
         elif turn == -1:
             pygame.draw.line(surface, color, (h*0.698, h*0.698), (h*0.835, h*0.835), 3)
             pygame.draw.line(surface, color, (h*0.835, h*0.698), (h*0.698, h*0.835), 3)
-        return 8
 
 pygame.init()
 size = width, height = 500, 500
@@ -115,24 +132,37 @@ map(height, screen, black)
 results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 font = pygame.font.SysFont('Comic Sans MS', 30)
 timer = 20
-
-while 1:
-
-    tekst = ""
-    for i in results:
-        tekst += str(i) + " "
-    cp_tekst = tekst
+running = True
+while running:
     for event in pygame.event.get():
-        screen.blit(font.render(tekst, False, (0, 0, 0)), (0, 0))
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            #sys.exit()
+            running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             poz = pygame.mouse.get_pos()
-            p = draw_object(height, screen, black, poz, turn)
-            if turn == 1 and p != None:
-                results[p] = 1
-            elif turn == -1 and p != None:
-                results[p] = -1
-            turn = -turn
+            num_field = check(height, poz)
+            if results[num_field] == 0:
+                draw_object(height, screen, black, turn)
+                if turn == 1 and num_field != None:
+                    results[num_field] = 1
+                elif turn == -1 and num_field != None:
+                    results[num_field] = -1
+
+
+                if (results[0] == results[3] == results[6] == 1) or (results[1] == results[4] == results[7] == 1) or \
+                    (results[2] == results[5] == results[8] == 1) or (results[0] == results[1] == results[2] == 1) or \
+                    (results[3] == results[4] == results[5] == 1) or (results[6] == results[7] == results[8] == 1) or \
+                    (results[0] == results[4] == results[8] == 1) or (results[2] == results[4] == results[6] == 1):
+                        screen.blit(font.render("Wygrało kółko", False, (0, 0, 0)), (0, 0))
+                elif (results[0] == results[3] == results[6] == -1) or (results[1] == results[4] == results[7] == -1) or \
+                    (results[2] == results[5] == results[8] == -1) or (results[0] == results[1] == results[2] == -1) or \
+                    (results[3] == results[4] == results[5] == -1) or (results[6] == results[7] == results[8] == -1) or \
+                    (results[0] == results[4] == results[8] == -1) or (results[2] == results[4] == results[6] == -1):
+                        screen.blit(font.render("Wygrał krzyżyk", False, (0, 0, 0)), (0, 0))
+
+
+                turn = -turn
+
 
 
 
