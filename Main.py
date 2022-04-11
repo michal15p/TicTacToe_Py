5fb1a6644047fb40f36d51e94cdf9fb06d6050c8
michal15p <103075090+michal15p@users.noreply.github.com>
2022-04-12 01:23:30 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,13 +22,13 @@ def draw_menu(h, w, surface, background):
 
 
 def op_menu(h, w, poz):
-    if poz[0] >= h * 0.200 and poz[0] < h * 0.600 and poz[1] >= w * 0.300 and poz[1] < w * 0.350:
+    if h * 0.200 <= poz[0] < h * 0.600 and w * 0.300 <= poz[1] < w * 0.350:
         return 0
-    if poz[0] >= h * 0.200 and poz[0] < h * 0.600 and poz[1] >= w * 0.400 and poz[1] < w * 0.450:
+    if h * 0.200 <= poz[0] < h * 0.600 and w * 0.400 <= poz[1] < w * 0.450:
         return 1
-    if poz[0] >= h * 0.200 and poz[0] < h * 0.600 and poz[1] >= w * 0.500 and poz[1] < w * 0.550:
+    if h * 0.200 <= poz[0] < h * 0.600 and w * 0.500 <= poz[1] < w * 0.550:
         return 2
-    if poz[0] >= h * 0.200 and poz[0] < h * 0.600 and poz[1] >= w * 0.600 and poz[1] < w * 0.650:
+    if h * 0.200 <= poz[0] < h * 0.600 and w * 0.600 <= poz[1] < w * 0.650:
         return 3
 
 
@@ -42,28 +42,25 @@ def map(h, surface, color, background):
 
 
 def check(h, poz):
-    if poz[0] >= h * 0.100 and poz[0] < h * 0.366 and poz[1] >= h * 0.100 and poz[1] < h * 0.366:
+    if h * 0.100 <= poz[0] < h * 0.366 and h * 0.100 <= poz[1] < h * 0.366:
         return 0
-    elif poz[0] >= h * 0.100 and poz[0] < h * 0.366 and poz[1] >= h * 0.366 and poz[1] < h * 0.633:
+    elif h * 0.100 <= poz[0] < h * 0.366 <= poz[1] < h * 0.633:
         return 1
-    elif poz[0] >= h * 0.100 and poz[0] < h * 0.366 and poz[1] >= h * 0.633 and poz[1] < h * 0.900:
+    elif h * 0.100 <= poz[0] < h * 0.366 and h * 0.633 <= poz[1] < h * 0.900:
         return 2
 
-
-
-    elif poz[0] >= h * 0.366 and poz[0] < h * 0.633 and poz[1] >= h * 0.100 and poz[1] < h * 0.366:
+    elif h * 0.633 > poz[0] >= h * 0.366 > poz[1] >= h * 0.100:
         return 3
-    elif poz[0] >= h * 0.366 and poz[0] < h * 0.633 and poz[1] >= h * 0.366 and poz[1] < h * 0.633:
+    elif h * 0.366 <= poz[0] < h * 0.633 and h * 0.366 <= poz[1] < h * 0.633:
         return 4
-    elif poz[0] >= h * 0.366 and poz[0] < h * 0.633 and poz[1] >= h * 0.633 and poz[1] < h * 0.900:
+    elif h * 0.366 <= poz[0] < h * 0.633 <= poz[1] < h * 0.900:
         return 5
 
-
-    elif poz[0] >= h * 0.633 and poz[0] < h * 0.900 and poz[1] >= h * 0.100 and poz[1] < h * 0.366:
+    elif h * 0.633 <= poz[0] < h * 0.900 and h * 0.100 <= poz[1] < h * 0.366:
         return 6
-    elif poz[0] >= h * 0.633 and poz[0] < h * 0.900 and poz[1] >= h * 0.366 and poz[1] < h * 0.633:
+    elif h * 0.900 > poz[0] >= h * 0.633 > poz[1] >= h * 0.366:
         return 7
-    elif poz[0] >= h * 0.633 and poz[0] < h * 0.900 and poz[1] >= h * 0.633 and poz[1] < h * 0.900:
+    elif h * 0.633 <= poz[0] < h * 0.900 and h * 0.633 <= poz[1] < h * 0.900:
         return 8
 
 
@@ -88,8 +85,6 @@ def draw_object(h, surface, color, turn, num_field):
             pygame.draw.line(surface, color, (h * 0.165, h * 0.698), (h * 0.300, h * 0.835), 3)
             pygame.draw.line(surface, color, (h * 0.300, h * 0.698), (h * 0.165, h * 0.835), 3)
 
-
-
     elif num_field == 3:
         if turn == 1:
             pygame.draw.circle(surface, color, (h * 0.499, h * 0.233), h * 0.100, 3)
@@ -108,7 +103,6 @@ def draw_object(h, surface, color, turn, num_field):
         elif turn == -1:
             pygame.draw.line(surface, color, (h * 0.430, h * 0.698), (h * 0.568, h * 0.835), 3)
             pygame.draw.line(surface, color, (h * 0.568, h * 0.698), (h * 0.430, h * 0.835), 3)
-
 
     elif num_field == 6:
         if turn == 1:
@@ -130,56 +124,59 @@ def draw_object(h, surface, color, turn, num_field):
             pygame.draw.line(surface, color, (h * 0.835, h * 0.698), (h * 0.698, h * 0.835), 3)
 
 
-def PvsP(height, screen, font, black, background):
+def pvsP(height, screen, font, black, background):
     turn = 1
     map(height, screen, black, background)
     results = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     pygame.time.set_timer(pygame.USEREVENT, 1000)
-    running_PvsP = True
-    while running_PvsP:
+    running_pvsp = True
+    end = False
+    while running_pvsp:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # sys.exit()
-                running_PvsP = False
+                running_pvsp = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                poz = pygame.mouse.get_pos()
-                num_field = check(height, poz)
-                if num_field != None:
-                    if results[num_field] == 0 and num_field != None:
-                        draw_object(height, screen, black, turn, num_field)
-                        if turn == 1:
-                            results[num_field] = 1
-                        elif turn == -1:
-                            results[num_field] = -1
-                        if (results[0] == results[3] == results[6] == 1) or (
-                                results[1] == results[4] == results[7] == 1) or \
-                                (results[2] == results[5] == results[8] == 1) or (
-                                results[0] == results[1] == results[2] == 1) or \
-                                (results[3] == results[4] == results[5] == 1) or (
-                                results[6] == results[7] == results[8] == 1) or \
-                                (results[0] == results[4] == results[8] == 1) or (
-                                results[2] == results[4] == results[6] == 1):
-                            screen.blit(font.render("Wygrało kółko", False, (0, 0, 0)), (0, 0))
-                            running_PvsP = False
+                if event.button == 1:
+                    poz = pygame.mouse.get_pos()
+                    num_field = check(height, poz)
+                    if num_field is not None:
+                        if results[num_field] == 0 and num_field is not None:
+                            draw_object(height, screen, black, turn, num_field)
+                            if turn == 1:
+                                results[num_field] = 1
+                            elif turn == -1:
+                                results[num_field] = -1
+                            if (results[0] == results[3] == results[6] == 1) or (
+                                    results[1] == results[4] == results[7] == 1) or \
+                                    (results[2] == results[5] == results[8] == 1) or (
+                                    results[0] == results[1] == results[2] == 1) or \
+                                    (results[3] == results[4] == results[5] == 1) or (
+                                    results[6] == results[7] == results[8] == 1) or \
+                                    (results[0] == results[4] == results[8] == 1) or (
+                                    results[2] == results[4] == results[6] == 1):
+                                screen.blit(font.render("Winner: Player 1 - click PPM to return", False, (0, 0, 0)), (0, 0))
+                                end = True
 
-                        elif (results[0] == results[3] == results[6] == -1) or (
-                                results[1] == results[4] == results[7] == -1) or \
-                                (results[2] == results[5] == results[8] == -1) or (
-                                results[0] == results[1] == results[2] == -1) or \
-                                (results[3] == results[4] == results[5] == -1) or (
-                                results[6] == results[7] == results[8] == -1) or \
-                                (results[0] == results[4] == results[8] == -1) or (
-                                results[2] == results[4] == results[6] == -1):
-                            screen.blit(font.render("Wygrał krzyżyk", False, (0, 0, 0)), (0, 0))
-                            running_PvsP = False
-                        if 0 not in results:
-                            screen.blit(font.render("Remis", False, (0, 0, 0)), (0, 0))
-                            running_PvsP = False
-                            # screen.fill(background)
-                        turn = -turn
+                            elif (results[0] == results[3] == results[6] == -1) or (
+                                    results[1] == results[4] == results[7] == -1) or \
+                                    (results[2] == results[5] == results[8] == -1) or (
+                                    results[0] == results[1] == results[2] == -1) or \
+                                    (results[3] == results[4] == results[5] == -1) or (
+                                    results[6] == results[7] == results[8] == -1) or \
+                                    (results[0] == results[4] == results[8] == -1) or (
+                                    results[2] == results[4] == results[6] == -1):
+                                screen.blit(font.render("Winner: Player 2 - click PPM to return", False, (0, 0, 0)), (0, 0))
+                                end = True
+                            if 0 not in results:
+                                screen.blit(font.render("Tie - click PPM to return", False, (0, 0, 0)), (0, 0))
+                                end = True
+                                # screen.fill(background)
+                            turn = -turn
+                elif event.button == 3 and end == True:
+                    running_pvsp = False
+                    draw_menu(height, width, screen, background)
         pygame.display.flip()
-    pygame.time.delay(3000)
-    draw_menu(height, width, screen, background)
 
 
 pygame.init()
@@ -198,6 +195,6 @@ while running_menu:
             poz = pygame.mouse.get_pos()
             choose = op_menu(height, width, poz)
             if choose == 0:
-                PvsP(height, screen, font, black, background)
+                pvsP(height, screen, font, black, background)
 
     pygame.display.flip()
